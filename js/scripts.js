252ecf7791c59bbba61c3c63e29f0a68f60c8c42
Kamil S <Figlarz@Figlarzs-MacBook-Pro.local>
2017-02-18 17:18:26 +0100

    with task 8.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 // plik scripts.js
 
 var name = prompt('Wpisz swoje imię');
+
+//wg zadania 8.3 komuntuje linie alert dowolnym type komentarza, zakładam, że o to chodzi :)
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+/* 
+nie czaje troche zadania, choc jest krotkie:
+1. W pliku scripts.js zakomentuj linię z poleceniem alert dowolnym typem komentarza.
+2. Wyślij zmiany na zdalne repozytorium.
+3. Dodaj jeszcze dwa inne komentarze.
+4. Stwórz nowe repozytorium i wyślij tam swój projekt. Pamiętaj o udostępnieniu go mentorowi na Githubie. Link do projektu wstaw poniżej.
+*/
+
+//dodaje jeszcze drugi komentarz i wysyłam dopiero na repozytorium, ale to z poprzedniego zadania bo rozni sie o te komentarze wiec chyban nie ma sensu zebym od początku robił całą strukturę i nowe repozytorium?
